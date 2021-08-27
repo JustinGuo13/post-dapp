69f09a2db5b9a686df8f7a66367626816d8fb4e5
Peng Yang Guo <pyangguo@gmail.com>
2021-08-27 15:01:13 -0700

    Implement contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -29,28 +29,64 @@ contract Voting {
 	State public state;
 
 	// Modifiers
+	modifier condition(bool _condition) {
+		require(_condition);
+		_;
+	}
 
+	modifier onlyOfficial(){
+		require(msg.sender == ballotOfficialAddress);
+		_;
+	}
+
+	modifier inState(State _state) {
+		require(state == _state);
+		_;
+	}
 	// Events
 
 	// Functions
 
-	constructor() {
+	constructor(string memory _ballotOfficialName, string memory _proposal) {
+		ballotOfficialAddress = msg.sender;
+		bollotOfficialName = _ballotOfficialName;
+		proposal = _proposal;
 
+		state = State.Created;
 	}
 
-	function addVoter() {
-
+	function addVoter(address _voterAddress, string memory _voterName) public inState(State.Created) onlyOfficial {
+		Voter memory v;
+		v.voterName = _voterName;
+		v.voted = false;
+		voterRegister[_voterAddress] = v;
+		totalVoter++;
 	}
 
-	function startVote() {
-
+	function startVote() public inState(State.Created) onlyOfficial {
+		state = State.Voting;
 	}
 
-	function doVote() {
+	function doVote(bool _choice) public inState(State.Voting) returns(bool voted) {
+			bool found = false;
+			if(bytes(voterRegister[msg.sender].voterName).length != 0 && !voterRegister[msg.sender].voted){
+					voterRegister[msg.sender].voted = true;
+					Vote memory v;
+					v.voterAddress = msg.sender;
+					v.choice = _choice;
 
+					if(_choice){
+						countResult++;
+					}
+					votes[totalVote] = v;
+					totalVote++;
+					found =true;
+			}
+			return found;
 	}
 
-	function endVote() {
-		
+	function endVote() public inState(State.Voting) onlyOfficial {
+		state = State.Ended;
+		finalResult = countResult;
 	}
 }
