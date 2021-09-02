686d208b754d81b245dc62e42e21a28de33d1820
Peng Yang Guo <pyangguo@gmail.com>
2021-09-02 14:53:06 -0700

    Connect React to contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Crud from './artifacts/contracts/Crud.sol/Crud.json';
+
+// Update with the contract address logged out to the CLI when it was deployed
+const crudAddress = 'your-contract-address';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// store crud in local state
+	const [crud, setCrudValue] = useState();
+
+	// request access to the user's MetaMask account
+	async function requestAccount() {
+		await window.ethereum.request({ method: 'eth_requestAccounts' });
+	}
+
+	// call the smart contract, create a post
+	async function createPost() {}
+
+	// call the smart contract, read post title and content
+	async function readPost() {}
+
+	// call the smart contract, edit post title and content
+	async function editPost() {}
+
+	// call the smart contract, delete post
+	async function deletePost() {}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
